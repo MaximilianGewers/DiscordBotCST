82d19b7164da8d26e2240926f99ff21918e324cd
CommanderDerpy <max.gewers@gmail.com>
2016-11-09 21:43:30 -0600

    Testing some Messaging
    Added some messaging for testing. It worked. There is a glitch though.
    If you repeat a command a second time the bot will send two answer. A
    third will give you three answer and so on.

--- Discord Bot/src/EventHandler.java
+++ Discord Bot/src/EventHandler.java
@@ -1,20 +1,56 @@
+import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.handle.impl.events.MessageSendEvent;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
 
-public class EventHandler
-{
-    @EventSubscriber
-    public void onReadyEvent( ReadyEvent event )
-    {
-        System.out.println("The bot is now ready");
-//        doSomething();
-    }
+public class EventHandler {
+	@EventSubscriber
+	public void onReadyEvent(ReadyEvent event) {
+		System.out.println("The bot is now ready");
+		// doSomething();
+	}
 
-    @EventSubscriber
-    public void onMessageEvent( MessageReceivedEvent event )
-    {
-        System.out.println(event.getMessage().getAuthor().getName() + ": "
-                + event.getMessage().getContent());
-    }
+	@EventSubscriber
+	public void onMessageEvent(MessageReceivedEvent event)
+			throws MissingPermissionsException, RateLimitException, DiscordException {
+		System.out.println(event.getMessage().getAuthor().getName() + ": " + event.getMessage().getContent());
+
+		// start of Example
+		/*
+		 * OnMessageEvent listens for a message and will than execute this code
+		 * The if statements will filter message content until it finds a true;
+		 */
+		if (event.getMessage().getContent().equalsIgnoreCase("ping")) {
+			//This sets the channel object to a static channel. The id here below
+			TestBot.channel = TestBot.discordClient.getChannelByID("238503415326441473");
+			//This is the message the bot will send in that channel
+			TestBot.channel.sendMessage("Pong");
+			// End of Example
+		} else if (event.getMessage().getContent().equalsIgnoreCase("!help")) {
+			TestBot.channel = TestBot.discordClient.getChannelByID("238503415326441473");
+			TestBot.channel.sendMessage("Sorry I can't do that yet");
+		} else if (event.getMessage().getContent().equalsIgnoreCase("who do you love the most. Me, Kale or Conner?")) {
+			TestBot.channel = TestBot.discordClient.getChannelByID("238503415326441473");
+			TestBot.channel.sendMessage("@CommanderDerpy#7862 of course. He is senpai. <3");
+		} else if (event.getMessage().getContent().equalsIgnoreCase("!LC")) {
+			TestBot.channel = TestBot.discordClient.getChannelByID("238503415326441473");
+			TestBot.channel.sendMessage("Testing channel list: " + TestBot.discordClient.getChannels());
+			TestBot.channel.sendMessage("Testing channel list: " + TestBot.channel.getID());
+		}
+		// else if (event.getMessage().getContent().equalsIgnoreCase("give
+		// current channel id")) {
+		// String channelID = TestBot.channel.getID();
+		//// work in progress TestBot.channel =
+		// TestBot.discordClient.getChannelByID("238503415326441473");
+		// TestBot.channel. = channelID;
+		// TestBot.channel.sendMessage("This channel called : `"
+		// +TestBot.channel.getName() +"` has the ID of : `"
+		// +TestBot.channel.getID()+"`");
+		// }
+	}
 }
--- Discord Bot/src/TestBot.java
+++ Discord Bot/src/TestBot.java
@@ -1,12 +1,12 @@
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.handle.impl.obj.Invite;
-import sx.blah.discord.handle.obj.IInvite;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.util.DiscordException;
 
 public class TestBot
 {
     public static IDiscordClient discordClient;
+    public static IChannel channel;
 
     public static void main( String[] args ) throws Exception
     {
@@ -14,16 +14,12 @@ public class TestBot
         {
             System.out.print("You need to specify a token before continuing");
         }
-        discordClient = getClient("XXXXXXXXXXX");
+        discordClient = getClient("XXX");
 
         discordClient.getDispatcher().registerListener(new EventHandler());
         discordClient.getDispatcher()
                 .registerListener(new ReadyEventListener());
-        
-        Invite inv = new Invite( discordClient, "FSVst");
-        inv.accept();
-        }
-
+    }
     public static IDiscordClient getClient( String token ) throws DiscordException
     {
         return new ClientBuilder().withToken(token).login();
